74e19da1b58ccd83844478dd46bbeb1a543c2365
Marco <pradamarco21@gmail.com>
2023-02-20 23:41:39 -0500

    persona= abstracta

--- Persona.java
+++ Persona.java
@@ -1,2 +1,55 @@
-public class Persona {
+public abstract class Persona {
+    public String nombre;
+    public String apellido;
+    public int id;
+    public String direccion;
+    public int telefono;
+
+    public Persona(String nombre, String apellido, int id, String direccion, int telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.id = id;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
 }
