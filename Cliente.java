70dc799ae8136b54d66df0accbfc59f7d2c0bd69
Angie <yulimatson28@gmail.com>
2023-02-20 23:59:10 -0500

    Cliente update

--- Cliente.java
+++ Cliente.java
@@ -1,2 +1,22 @@
-public class Cliente {
+public class Cliente  extends  Persona{
+    float peso = 0;
+    public Cliente(String nombre, String apellido, int id, String direccion, int telefono, float peso) {
+        super(nombre, apellido, id, direccion, telefono);
+        this.peso = peso;
+    }
+    public float getPeso() {
+        return peso;
+    }
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+    public void pesomax(float peso){
+        if(peso <= 150){
+            System.out.println("Peso Válido por persona");
+            }
+        else{
+            System.out.println("Peso No Válido, no puede ingresar");
+        }
+
+    }
 }
