b27dbcfd0b49463a2e6966aeb67ed270df004f0b
Marco <pradamarco21@gmail.com>
2023-02-25 02:41:53 -0500

    clase registro extiende tiquete

--- com/upb/edu/FerroUPBP1/Model/Registro/Registro.java
+++ com/upb/edu/FerroUPBP1/Model/Registro/Registro.java
@@ -1,8 +1,12 @@
 package com.upb.edu.FerroUPBP1.Model.Registro;
+
+import com.upb.edu.FerroUPBP1.Model.Persona.Cliente;
 import com.upb.edu.FerroUPBP1.Model.Tiquete.Tiquete;
 
+public class Registro extends Tiquete {
 
-public class Registro implements Tiquete{
 
+    public Registro(int precioVenta, String tipoTiquete, boolean chequeoTiquete, Cliente cliente) {
+        super(precioVenta, tipoTiquete, chequeoTiquete, cliente);
     }
 }
