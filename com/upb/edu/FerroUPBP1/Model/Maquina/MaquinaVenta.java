f10a704e31d536ed95c610e27d71b72634d1fe5f
Angie <yulimatson28@gmail.com>
2023-02-25 03:46:59 -0500

    Main MaquinaVenta

--- com/upb/edu/FerroUPBP1/Model/Maquina/MaquinaVenta.java
+++ com/upb/edu/FerroUPBP1/Model/Maquina/MaquinaVenta.java
@@ -1,24 +1,30 @@
 package com.upb.edu.FerroUPBP1.Model.Maquina;
 import com.upb.edu.FerroUPBP1.Model.Persona.Cliente;
 import com.upb.edu.FerroUPBP1.Model.Tiquete.Tiquete;
+
+import javax.swing.*;
+
 public class MaquinaVenta extends Tiquete{
     public MaquinaVenta(int precioVenta, String tipoTiquete, boolean chequeoTiquete, Cliente cliente) {
         super(precioVenta, tipoTiquete, chequeoTiquete, cliente);
     }
     public static void main(String[] args) {
 
-
+        JOptionPane.showMessageDialog(null,"Ingresar Datos");
+        String nombres = JOptionPane.showInputDialog(null,"Escribir los Nombres");
+        String apellidos = JOptionPane.showInputDialog(null,"Escribir los Apellidos");
+        JOptionPane.showMessageDialog(null,"Bienvenido " + nombres + " " + apellidos);
+        String direccion = JOptionPane.showInputDialog(null,"Escribir la dirección");
+        int telefono = Integer.parseInt(JOptionPane.showInputDialog(null,"Escribir el teléfono"));
+        int peso = Integer.parseInt(JOptionPane.showInputDialog(null,"Escribir cantidad de peso a llevar"));
 
 
     }
-
     public boolean venta(boolean chequeoTiquete, float peso){
         if (chequeoTiquete == true){
             return true;
         } else {
             return false;
-        } if (Cliente.pesomax(peso)){
-
         }
     }
 
