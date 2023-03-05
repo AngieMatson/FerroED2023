2d729c26bfbf886cfd87cae3f637ac04061b24b9
Marco <pradamarco21@gmail.com>
2023-03-04 20:57:46 -0500

    intente hacer algo en tren y vagon pero no pude xd

--- com/upb/edu/FerroUPBP1/Model/Tren/Tren.java
+++ com/upb/edu/FerroUPBP1/Model/Tren/Tren.java
@@ -1,4 +1,16 @@
 package com.upb.edu.FerroUPBP1.Model.Tren;
 
+import com.upb.edu.FerroUPBP1.Lista.LinkedList;
+
 public abstract class Tren {
+    Vagon xd;
+    LinkedList<Object> ListaVagon = new LinkedList<Object>();
+    public void NumeroVagones(){
+        if (xd.verificarNumPasajeros()){
+            ListaVagon.add(xd);
+        }
+    }
+
+
+
 }
--- com/upb/edu/FerroUPBP1/Model/Tren/Vagon.java
+++ com/upb/edu/FerroUPBP1/Model/Tren/Vagon.java
@@ -1,4 +1,25 @@
 package com.upb.edu.FerroUPBP1.Model.Tren;
 
+import com.upb.edu.FerroUPBP1.Lista.LinkedList;
+import com.upb.edu.FerroUPBP1.Model.Persona.Cliente;
+
 public class Vagon {
+    Cliente pasajero;
+    LinkedList<Cliente> listaPasajero = new LinkedList<Cliente>();
+    public boolean verificarNumPasajeros(){
+        if (listaPasajero.size()>=30){
+            return false;
+        }
+        else{return true;}
+    }
+    public void añadirPasajerosAlVagon(){
+        if (listaPasajero.size()>=30) {
+            listaPasajero.add(pasajero);
+        }
+        else{System.out.println("El Vagon esta lleno");}
+
+    }
+
+
+
 }
